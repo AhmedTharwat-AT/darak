603479f5deff7e0a65d83b5bf810961c0fee8d13
iMorphex <ahmedsarwat3000@yahoo.com>
2024-11-13 13:32:07 +0200

    debug revalidate endpoint

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -7,6 +7,15 @@ export async function POST(request: NextRequest) {
   console.log("revalidate route : ", secret);
   console.log("request url : ", request.url);
 
+  // Get the host from the headers
+  const host = request.headers.get("host");
+
+  // Get the origin from the headers
+  const origin = request.headers.get("origin");
+
+  console.log("host : ", host);
+  console.log("origin : ", origin);
+
   // Check for secret to confirm this is a valid request
   if (secret !== process.env.REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
